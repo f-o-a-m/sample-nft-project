986202307d71cbf56bba854adca94880e48dcd9d
Carl Factora <IvantheTricourne@users.noreply.github.com>
2019-08-22 16:12:37 -0400

    Clean file

--- contracts/SignalMarket.sol
+++ contracts/SignalMarket.sol
@@ -2,14 +2,13 @@ pragma solidity ^0.4.26;
 
 import "./FoamToken.sol";
 import "./SignalToken.sol";
-import "./SimpleStorage.sol";
 
 contract SignalMarket {
-  // @TODO replace with real token types
   FoamToken public foamToken;
   SignalToken public signalToken;
 
   constructor(address _signalToken, address _foamToken) public {
+    // these are basically type conversions; they are _not_ constructors
     foamToken = FoamToken(_foamToken);
     signalToken = SignalToken(_signalToken);
   }
