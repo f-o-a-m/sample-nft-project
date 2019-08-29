11e6144f3606e5ddef6c9c16af6de8297fc0c8d6
Carl Factora <cfactora93@gmail.com>
2019-08-29 13:53:21 -0400

    Add SignalMarket.forSale function

--- contracts/SignalMarket.sol
+++ contracts/SignalMarket.sol
@@ -2,10 +2,23 @@ pragma solidity ^0.4.26;
 
 import "./FoamToken.sol";
 import "./SignalToken.sol";
+import "openzeppelin-solidity/contracts/token/ERC721/ERC721Holder.sol";
 
-contract SignalMarket {
+contract SignalMarket is ERC721Holder {
+
+  struct Sale {
+    uint256 signalId;
+    uint256 price;
+    address owner;
+  }
+
+  Sale[] public sales;
   FoamToken public foamToken;
   SignalToken public signalToken;
+
+  mapping(uint256 => uint256) public signalToSale;
+
+  event SignalForSale(uint256 signalId, uint256 price, uint256 saleId);
 
   constructor(address _signalToken, address _foamToken) public {
     // these are basically type conversions; they are _not_ constructors
@@ -13,13 +26,36 @@ contract SignalMarket {
     signalToken = SignalToken(_signalToken);
   }
 
-  function markTokenForSale(address owner, uint256 tokenID, uint256 price) public returns (uint256 tokenID) {
-    // @TODO: need to check if token is actually owned by owner
-    return tokenID;
+  function forSale(uint256 _tokenId, uint256 _price) public {
+    // @NOTE: the SignalToken contract has to approve _this_ contract
+    // to do this?
+
+    // Check for caller ownership
+    require(signalToken.ownerOf(_tokenId) == msg.sender);
+
+    // given ownership to SignalMarket
+    signalToken.safeTransferFrom(msg.sender, address(this), _tokenId);
+
+    // create sale struct
+    Sale memory s = Sale({
+      signalId: _tokenId,
+      price: _price,
+      owner: msg.sender
+    });
+
+    // make saleId
+    uint256 saleId = sales.push(s) - 1;
+
+    signalToSale[_tokenId] = saleId;
+
+    // emit new sale
+    emit SignalForSale(_tokenId, _price, saleId);
   }
 
-  function buyToken(uint256 tokenID, uint256 payment) public {
-    // maybe some sort of owner transfer
-  }
+  /* function buy(uint256 tokenID, uint256 payment) public { */
+  /*   // some sort of owner transfer via the SignalToken.transfer? */
+  /*   // if token is for sale, the token should already be approved */
+  /*   // for working in this contract */
+  /* } */
 
 }
--- src/Contracts/SignalMarket.purs
+++ src/Contracts/SignalMarket.purs
@@ -8,11 +8,19 @@ import Prelude
 
 import Data.Either (Either)
 import Data.Functor.Tagged (Tagged, tagged)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Eq (genericEq)
+import Data.Generic.Rep.Show (genericShow)
+import Data.Lens ((.~))
+import Data.Maybe (Maybe(..), fromJust)
+import Data.Newtype (class Newtype)
 import Data.Symbol (SProxy)
-import Network.Ethereum.Web3 (call, deployContract)
+import Network.Ethereum.Web3 (_address, _topics, call, class EventFilter, deployContract, sendTx)
 import Network.Ethereum.Web3.Contract.Internal (uncurryFields)
-import Network.Ethereum.Web3.Solidity (Tuple0(..), Tuple2(..), unTuple1)
-import Network.Ethereum.Web3.Types (Address, CallError, ChainCursor, HexString, NoPay, TransactionOptions, Web3)
+import Network.Ethereum.Web3.Solidity (ByteString, D2, D5, D6, DOne, Tuple0(..), Tuple1(..), Tuple2(..), Tuple3, Tuple4(..), UIntN, class IndexedEvent, unTuple1)
+import Network.Ethereum.Web3.Solidity.Size (type (:&))
+import Network.Ethereum.Web3.Types (Address, CallError, ChainCursor, HexString, NoPay, TransactionOptions, Web3, defaultFilter, mkHexString)
+import Partial.Unsafe (unsafePartial)
 --------------------------------------------------------------------------------
 -- | ConstructorFn
 --------------------------------------------------------------------------------
@@ -27,6 +35,31 @@ constructor x0 bc r = uncurryFields  r $ constructor' x0 bc
     constructor' y0 bc' y2 y3 = deployContract y0 bc' ((tagged $ Tuple2 y2 y3) :: ConstructorFn)
 
 --------------------------------------------------------------------------------
+-- | SignalForSale
+--------------------------------------------------------------------------------
+
+
+newtype SignalForSale = SignalForSale {signalId :: (UIntN (D2 :& D5 :& DOne D6)),price :: (UIntN (D2 :& D5 :& DOne D6)),saleId :: (UIntN (D2 :& D5 :& DOne D6))}
+
+derive instance newtypeSignalForSale :: Newtype SignalForSale _
+
+instance eventFilterSignalForSale :: EventFilter SignalForSale where
+	eventFilter _ addr = defaultFilter
+		# _address .~ Just addr
+		# _topics .~ Just [Just ( unsafePartial $ fromJust $ mkHexString "f9e67756bf381d7d896f3a05c6b13c39bbe9bd264cc0ae3edc9b9e0ccdb76a36")]
+
+instance indexedEventSignalForSale :: IndexedEvent (Tuple0 ) (Tuple3 (Tagged (SProxy "signalId") (UIntN (D2 :& D5 :& DOne D6))) (Tagged (SProxy "price") (UIntN (D2 :& D5 :& DOne D6))) (Tagged (SProxy "saleId") (UIntN (D2 :& D5 :& DOne D6)))) SignalForSale where
+  isAnonymous _ = false
+
+derive instance genericSignalForSale :: Generic SignalForSale _
+
+instance eventGenericSignalForSaleShow :: Show SignalForSale where
+	show = genericShow
+
+instance eventGenericSignalForSaleeq :: Eq SignalForSale where
+	eq = genericEq
+
+--------------------------------------------------------------------------------
 -- | FoamTokenFn
 --------------------------------------------------------------------------------
 
@@ -35,6 +68,49 @@ type FoamTokenFn = Tagged (SProxy "foamToken()") (Tuple0 )
 
 foamToken :: TransactionOptions NoPay -> ChainCursor -> Web3 (Either CallError Address)
 foamToken x0 cm = map unTuple1 <$> call x0 cm ((tagged $ Tuple0 ) :: FoamTokenFn)
+
+--------------------------------------------------------------------------------
+-- | ForSaleFn
+--------------------------------------------------------------------------------
+
+
+type ForSaleFn = Tagged (SProxy "forSale(uint256,uint256)") (Tuple2 (Tagged (SProxy "_tokenId") (UIntN (D2 :& D5 :& DOne D6))) (Tagged (SProxy "_price") (UIntN (D2 :& D5 :& DOne D6))))
+
+forSale :: TransactionOptions NoPay -> { _tokenId :: (UIntN (D2 :& D5 :& DOne D6)), _price :: (UIntN (D2 :& D5 :& DOne D6)) } -> Web3 HexString
+forSale x0 r = uncurryFields  r $ forSale' x0
+   where
+    forSale' :: TransactionOptions NoPay -> (Tagged (SProxy "_tokenId") (UIntN (D2 :& D5 :& DOne D6))) -> (Tagged (SProxy "_price") (UIntN (D2 :& D5 :& DOne D6))) -> Web3 HexString
+    forSale' y0 y1 y2 = sendTx y0 ((tagged $ Tuple2 y1 y2) :: ForSaleFn)
+
+--------------------------------------------------------------------------------
+-- | OnERC721ReceivedFn
+--------------------------------------------------------------------------------
+
+
+type OnERC721ReceivedFn = Tagged (SProxy "onERC721Received(address,address,uint256,bytes)") (Tuple4 Address Address (UIntN (D2 :& D5 :& DOne D6)) ByteString)
+
+onERC721Received :: TransactionOptions NoPay -> Address -> Address -> (UIntN (D2 :& D5 :& DOne D6)) -> ByteString -> Web3 HexString
+onERC721Received x0 x1 x2 x3 x4 = sendTx x0 ((tagged $ Tuple4 x1 x2 x3 x4) :: OnERC721ReceivedFn)
+
+--------------------------------------------------------------------------------
+-- | SalesFn
+--------------------------------------------------------------------------------
+
+
+type SalesFn = Tagged (SProxy "sales(uint256)") (Tuple1 (UIntN (D2 :& D5 :& DOne D6)))
+
+sales :: TransactionOptions NoPay -> ChainCursor -> (UIntN (D2 :& D5 :& DOne D6)) -> Web3 (Either CallError (Tuple3 (UIntN (D2 :& D5 :& DOne D6)) (UIntN (D2 :& D5 :& DOne D6)) Address))
+sales x0 cm x2 = call x0 cm ((tagged $ Tuple1 x2) :: SalesFn)
+
+--------------------------------------------------------------------------------
+-- | SignalToSaleFn
+--------------------------------------------------------------------------------
+
+
+type SignalToSaleFn = Tagged (SProxy "signalToSale(uint256)") (Tuple1 (UIntN (D2 :& D5 :& DOne D6)))
+
+signalToSale :: TransactionOptions NoPay -> ChainCursor -> (UIntN (D2 :& D5 :& DOne D6)) -> Web3 (Either CallError (UIntN (D2 :& D5 :& DOne D6)))
+signalToSale x0 cm x2 = map unTuple1 <$> call x0 cm ((tagged $ Tuple1 x2) :: SignalToSaleFn)
 
 --------------------------------------------------------------------------------
 -- | SignalTokenFn
