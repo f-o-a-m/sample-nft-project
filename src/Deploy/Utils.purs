f2dd1618bd30315a65da35f8a2fa19cd0d96df21
Carl Factora <cfactora93@gmail.com>
2019-09-04 15:58:52 -0400

    Add pair programming code

--- src/Deploy/Utils.purs
+++ src/Deploy/Utils.purs
@@ -3,12 +3,12 @@ module Deploy.Utils where
 import Prelude
 
 import Chanterelle.Internal.Utils.Web3 (pollTransactionReceipt)
-import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff)
 import Network.Ethereum.Web3 (HexString, Provider, TransactionReceipt(..), TransactionStatus(..))
 import Partial.Unsafe (unsafeCrashWith)
 
 -- @TODO: make an issue to add this to chanterelle (Chanterelle.Test module)
-awaitTxSuccess :: HexString -> Provider -> Aff Unit
+awaitTxSuccess :: forall m. MonadAff m => HexString -> Provider -> m Unit
 awaitTxSuccess txHash provider = do
   TransactionReceipt txReceipt <- pollTransactionReceipt txHash provider
   case txReceipt.status of
--- test/Test/SignalMarketSpec.purs
+++ test/Test/SignalMarketSpec.purs
@@ -8,26 +8,127 @@ import Chanterelle.Test (TestConfig, assertWeb3, takeEvent)
 import Contracts.FoamToken as FoamToken
 import Contracts.SignalMarket as SignalMarket
 import Contracts.SignalToken as SignalToken
+import Control.Monad.Reader (ask)
 import Control.Parallel (parTraverse_)
 import Data.Array ((!!))
 import Data.Lens ((?~))
 import Data.Maybe (fromJust)
 import Data.Newtype (unwrap)
 import Data.Tuple (Tuple(..))
-import Effect.Aff (Aff)
 import Deploy.Main (SignalMarket, SignalToken)
+import Deploy.Utils (awaitTxSuccess)
+import Effect.Aff (Aff)
+import Effect.Aff.Class (class MonadAff, liftAff)
 import Network.Ethereum.Core.HexString (mkHexString)
 import Network.Ethereum.Web3 (Address, ChainCursor(..), Ether, HexString, Provider, Value, Web3, _from, _gas, _to, _value, convert, defaultTransactionOptions, embed, mkAddress, mkValue, unUIntN)
 import Network.Ethereum.Web3.Api (eth_sendTransaction)
 import Network.Ethereum.Web3.Solidity.Sizes (s256, s32)
 import Partial.Unsafe (unsafePartial)
-import Test.Spec (SpecT, before, beforeAll_, describe, it)
+import Test.Spec (SpecT, before, beforeAll_, describe, it, parallel)
 import Test.Spec.Assertions (shouldEqual, shouldSatisfy)
+import Test.Utils (assertStorageCall, assertWeb3Test, go, mkBytesN, mkUIntN, unsafeFromJust)
 import Type.Proxy (Proxy(..))
-import Deploy.Utils (awaitTxSuccess)
-import Test.Utils (assertStorageCall, mkBytesN, mkUIntN, unsafeFromJust)
 
--- import Effect.Class.Console (log)
+type FilterEnv m =
+  { logger :: String -> m Unit
+  }
+
+type SignalMarketTestCfg r =
+  ( foamToken :: DeployReceipt NoArgs
+  , signalToken :: DeployReceipt SignalToken
+  , tokenFaucet :: Address
+  , signalMarket :: DeployReceipt SignalMarket | r
+  )
+
+spec
+  :: forall r.
+     TestConfig (SignalMarketTestCfg r)
+  -> SpecT Aff Unit Aff Unit
+spec cfg =
+  let env = { logger: \s -> ask >>= \logger -> liftAff $ logger s
+            }
+  in go $ spec' cfg env
+
+spec'
+  :: forall r m.
+     MonadAff m
+  => TestConfig (SignalMarketTestCfg r)
+  -> FilterEnv m
+  -> SpecT m Unit Aff Unit
+spec' testCfg _ = do
+  describe "interact with signal market" $ parallel do
+    let zeroAddr = unsafeFromJust "Must be valid Address 000..." $
+                   mkAddress =<< mkHexString "0x0000000000000000000000000000000000000000"
+        { provider
+        , accounts
+        , foamToken: { deployAddress: foamToken }
+        , signalToken: { deployAddress: signalToken }
+        , signalMarket: { deployAddress: signalMarket }
+        , tokenFaucet
+        } = testCfg
+        -- set up 2 accounts
+        account1 = unsafePartial $ fromJust $ accounts !! 1
+        account2 = unsafePartial $ fromJust $ accounts !! 2
+    beforeAll_ (faucetTokens { foamToken, tokenFaucet, provider, account1, account2 }) $ do
+      it "can run the faucet" do
+        let txOpts = defaultTransactionOptions # _to ?~ foamToken
+        a1balance <- assertStorageCall provider $
+                     FoamToken.balanceOf txOpts Latest { _owner: account1 }
+        a2balance <- assertStorageCall provider $
+                     FoamToken.balanceOf txOpts Latest { _owner: account2 }
+        unUIntN a1balance `shouldSatisfy` (_ > zero)
+        unUIntN a2balance `shouldSatisfy` (_ > zero)
+        -- @TODO (maybe): check acc2 ETH balance
+
+      -- @NOTE: at this point all contracts are already deployed
+      -- to test for a successfully deployed contract, verify that
+      -- all global get functions are pointed to the correct contract addresses
+      it "can verify the signal market is deployed" do
+        let txOpts = defaultTransactionOptions # _to ?~ signalMarket
+        -- global constructor calls
+        foamTokenAddr <- assertStorageCall provider $
+                         SignalMarket.foamToken txOpts Latest
+        signalTokenAddr <- assertStorageCall provider $
+                           SignalMarket.signalToken txOpts Latest
+        foamTokenAddr `shouldEqual` foamToken
+        signalTokenAddr `shouldEqual` signalTokenAddr
+
+      let approveAndMintArgs = { foamToken, signalToken, provider, account1 }
+      before (approveAndMintSignal approveAndMintArgs) $ do
+        it "can mint a signal token (ERC-721)" \{ mintTransfer } -> do
+          let minted = unwrap mintTransfer
+          -- verify ownership/transfer
+          minted._to `shouldEqual` account1
+          -- a newly minted signal is always from the `zeroAddr`
+          minted._from `shouldEqual` zeroAddr
+
+      before (do
+          { mintTransfer } <- approveAndMintSignal approveAndMintArgs
+          markSignalForSale { mintTransfer, signalToken, signalMarket, provider, account1 }
+        ) $ do
+        let originalPrice = mkUIntN s256 1
+        it "can mark signal tokens for sale" \{ mintTransfer, signalForSale } -> do
+          -- check price and Id value
+          let s = unwrap signalForSale
+              m = unwrap mintTransfer
+          s.price `shouldEqual` originalPrice
+          s.signalId `shouldEqual` m._tokenId
+
+        it "can buy signal tokens" \{ signalForSale } -> do
+          let txOpts = defaultTransactionOptions # _from ?~ account2
+                                                 # _gas ?~ embed 8000000
+              signal = unwrap signalForSale
+              acc2BuyAction _tokenId =
+                SignalMarket.buy (txOpts # _to ?~ signalMarket
+                                         # _value ?~ convert (mkValue one :: Value Ether)) { _tokenId }
+          -- make account2 buy the signal from account1
+          Tuple _ (SignalMarket.SignalSold purchase) <- assertWeb3 provider $
+            takeEvent (Proxy :: Proxy SignalMarket.SignalSold) signalMarket $ acc2BuyAction signal.signalId
+          -- check sale details and transfer of ownership
+          purchase.signalId `shouldEqual` signal.signalId
+          purchase.price `shouldEqual` originalPrice
+          purchase.owner `shouldEqual` account1
+          purchase.newOwner `shouldEqual` account2
 
 faucet
   :: { recipient :: Address
@@ -66,10 +167,10 @@ faucetTokens
 faucetTokens { foamToken, tokenFaucet, provider, account1, account2 } = do
   -- give FOAM tokens to each of them (via faucet)
   flip parTraverse_ [account1, account2] \recipient -> do
-    txHash <- assertWeb3 provider $ faucet { recipient, foamToken, tokenFaucet }
+    txHash <- assertWeb3Test provider $ faucet { recipient, foamToken, tokenFaucet }
     awaitTxSuccess txHash provider
   -- give one ETH to account2
-  txHash <- assertWeb3 provider $ ethFaucetOne { recipient: account2
+  txHash <- assertWeb3Test provider $ ethFaucetOne { recipient: account2
                                                , tokenFaucet
                                                }
   awaitTxSuccess txHash provider
@@ -142,84 +243,3 @@ markSignalForSale { signalToken, signalMarket, mintTransfer, provider, account1 
     takeEvent (Proxy :: Proxy SignalMarket.SignalForSale) signalMarket $ forSaleAction
 
   pure { mintTransfer, signalForSale }
-
-spec
-  :: forall r .
-     TestConfig ( foamToken :: DeployReceipt NoArgs
-                , signalToken :: DeployReceipt SignalToken
-                , tokenFaucet :: Address
-                , signalMarket :: DeployReceipt SignalMarket | r)
-  -> SpecT Aff Unit Aff Unit
-spec { provider
-     , accounts
-     , foamToken: {deployAddress: foamToken}
-     , signalToken: {deployAddress: signalToken}
-     , signalMarket: {deployAddress: signalMarket}
-     , tokenFaucet
-     } = do
-  describe "interact with signal market" do
-    let zeroAddr = unsafeFromJust "Must be valid Address 000..." $
-                   mkAddress =<< mkHexString "0x0000000000000000000000000000000000000000"
-        -- set up 2 accounts
-        account1 = unsafePartial $ fromJust $ accounts !! 1
-        account2 = unsafePartial $ fromJust $ accounts !! 2
-    beforeAll_ (faucetTokens { foamToken, tokenFaucet, provider, account1, account2 }) $ do
-      it "can run the faucet" do
-        let txOpts = defaultTransactionOptions # _to ?~ foamToken
-        a1balance <- assertStorageCall provider $
-                     FoamToken.balanceOf txOpts Latest { _owner: account1 }
-        a2balance <- assertStorageCall provider $
-                     FoamToken.balanceOf txOpts Latest { _owner: account2 }
-        unUIntN a1balance `shouldSatisfy` (_ > zero)
-        unUIntN a2balance `shouldSatisfy` (_ > zero)
-        -- @TODO (maybe): check acc2 ETH balance
-
-      -- @NOTE: at this point all contracts are already deployed
-      -- to test for a successfully deployed contract, verify that
-      -- all global get functions are pointed to the correct contract addresses
-      it "can verify the signal market is deployed" do
-        let txOpts = defaultTransactionOptions # _to ?~ signalMarket
-        -- global constructor calls
-        foamTokenAddr <- assertStorageCall provider $
-                         SignalMarket.foamToken txOpts Latest
-        signalTokenAddr <- assertStorageCall provider $
-                           SignalMarket.signalToken txOpts Latest
-        foamTokenAddr `shouldEqual` foamToken
-        signalTokenAddr `shouldEqual` signalTokenAddr
-
-      let approveAndMintArgs = { foamToken, signalToken, provider, account1 }
-      before (approveAndMintSignal approveAndMintArgs) $ do
-        it "can mint a signal token (ERC-721)" \{ mintTransfer } -> do
-          let minted = unwrap mintTransfer
-          -- verify ownership/transfer
-          minted._to `shouldEqual` account1
-          -- a newly minted signal is always from the `zeroAddr`
-          minted._from `shouldEqual` zeroAddr
-
-      before (do
-          { mintTransfer } <- approveAndMintSignal approveAndMintArgs
-          markSignalForSale { mintTransfer, signalToken, signalMarket, provider, account1 }
-        ) $ do
-        let originalPrice = mkUIntN s256 1
-        it "can mark signal tokens for sale" \{ mintTransfer, signalForSale } -> do
-          -- check price and Id value
-          let s = unwrap signalForSale
-              m = unwrap mintTransfer
-          s.price `shouldEqual` originalPrice
-          s.signalId `shouldEqual` m._tokenId
-
-        it "can buy signal tokens" \{ signalForSale } -> do
-          let txOpts = defaultTransactionOptions # _from ?~ account2
-                                                 # _gas ?~ embed 8000000
-              signal = unwrap signalForSale
-              acc2BuyAction _tokenId =
-                SignalMarket.buy (txOpts # _to ?~ signalMarket
-                                         # _value ?~ convert (mkValue one :: Value Ether)) { _tokenId }
-          -- make account2 buy the signal from account1
-          Tuple _ (SignalMarket.SignalSold purchase) <- assertWeb3 provider $
-            takeEvent (Proxy :: Proxy SignalMarket.SignalSold) signalMarket $ acc2BuyAction signal.signalId
-          -- check sale details and transfer of ownership
-          purchase.signalId `shouldEqual` signal.signalId
-          purchase.price `shouldEqual` originalPrice
-          purchase.owner `shouldEqual` account1
-          purchase.newOwner `shouldEqual` account2
